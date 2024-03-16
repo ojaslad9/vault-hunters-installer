3f9281a9ca17ff8534993e5ab3d1c6e1ebc640b9
ojaslad9 <163666554+ojaslad9@users.noreply.github.com>
2024-03-16 21:39:46 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
+apt install wget unzip openjdk-17-jre -y
 wget https://www.curseforge.com/api/v1/mods/711537/files/5076205/download
 mv download download.zip
 unzip download.zip
 wget https://maven.minecraftforge.net/net/minecraftforge/forge/1.18.2-40.2.9/forge-1.18.2-40.2.9-installer.jar
 apt update
-apt install openjdk-17-jre -y
 java -jar forge-1.18.2-40.2.9-installer.jar --installServer
